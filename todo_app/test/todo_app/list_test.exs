cbc7c92d1492bf3a5599afac9af765be9f5a9041
thomas.eberl <thomas.eberl@check24.de>
2018-11-13 17:42:57 +0100

    Closes #1
    * Implement Collectable protocoll
    * Add tests for Enum.into usage

--- todo_app/test/todo_app/list_test.exs
+++ todo_app/test/todo_app/list_test.exs
@@ -9,25 +9,60 @@ defmodule TodoApp.ListTest do
   end
 
   test "add/2 adds a todo to an existing list" do
+    expected_result = %List{
+      auto_id: 3,
+      entries: %{
+        1 => %Todo{
+          completed: false,
+          date: ~D[1984-01-01],
+          id: 1,
+          title: "Buy tomatoes."
+        },
+        2 => %Todo{
+          completed: false,
+          date: ~D[1984-01-01],
+          id: 2,
+          title: "Buy cucumbers."
+        }
+      }
+    }
+
     assert List.new()
            |> List.add(Todo.new(~D[1984-01-01], "Buy tomatoes."))
-           |> List.add(Todo.new(~D[1984-01-01], "Buy cucumbers.")) ==
-             %List{
-               auto_id: 3,
-               entries: %{
-                 1 => %Todo{
-                   completed: false,
-                   date: ~D[1984-01-01],
-                   id: 1,
-                   title: "Buy tomatoes."
-                 },
-                 2 => %Todo{
-                   completed: false,
-                   date: ~D[1984-01-01],
-                   id: 2,
-                   title: "Buy cucumbers."
-                 }
-               }
-             }
+           |> List.add(Todo.new(~D[1984-01-01], "Buy cucumbers.")) == expected_result
+  end
+
+  test "Enum.into/2 adds the todos to the list" do
+    todos = [
+      Todo.new(~D[1984-01-01], "Buy fish."),
+      Todo.new(~D[1984-01-01], "Buy chips."),
+      Todo.new(~D[1984-01-01], "Buy beer.")
+    ]
+
+    expected_result = %List{
+      auto_id: 4,
+      entries: %{
+        1 => %Todo{
+          completed: false,
+          date: ~D[1984-01-01],
+          id: 1,
+          title: "Buy fish."
+        },
+        2 => %Todo{
+          completed: false,
+          date: ~D[1984-01-01],
+          id: 2,
+          title: "Buy chips."
+        },
+        3 => %Todo{
+          completed: false,
+          date: ~D[1984-01-01],
+          id: 3,
+          title: "Buy beer."
+        }
+      }
+    }
+
+    assert Enum.into(todos, List.new()) == expected_result
   end
 end
