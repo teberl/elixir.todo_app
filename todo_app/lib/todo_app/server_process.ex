737276f3e76b52e0a2e63981b5290044814c0e79
teberl <mail@teberl.de>
2018-11-18 12:01:40 +0100

    Add async supprt with cast functions

--- todo_app/lib/todo_app/server_process.ex
+++ todo_app/lib/todo_app/server_process.ex
@@ -7,7 +7,7 @@ defmodule TodoApp.ServerProcess do
   end
 
   def call(server_pid, request) do
-    send(server_pid, {request, self()})
+    send(server_pid, {:call, request, self()})
 
     receive do
       {:response, response} ->
@@ -15,9 +15,14 @@ defmodule TodoApp.ServerProcess do
     end
   end
 
+  def cast(server_pid, request) do
+    send(server_pid, {:cast, request})
+    :ok
+  end
+
   defp loop(callback_module, current_state) do
     receive do
-      {request, caller} ->
+      {:call, request, caller} ->
         {response, new_state} =
           callback_module.handle_call(
             request,
@@ -25,6 +30,14 @@ defmodule TodoApp.ServerProcess do
           )
 
         send(caller, {:response, response})
+        loop(callback_module, new_state)
+
+      {:cast, request} ->
+        new_state =
+          callback_module.handle_cast(
+            request,
+            current_state
+          )
 
         loop(callback_module, new_state)
     end
--- todo_app/lib/todo_app/todo_store.ex
+++ todo_app/lib/todo_app/todo_store.ex
@@ -2,35 +2,53 @@ defmodule TodoApp.TodoStore do
   alias __MODULE__
   alias TodoApp.{ServerProcess, TodoList, TodoEntry}
 
+  # INTERFACE FUNCTIONS FOR THE CLIENT
   def start() do
     ServerProcess.start(TodoStore)
   end
 
   def get(pid) do
-    ServerProcess.call(pid, {:get})
+    ServerProcess.call(pid, {:get_all})
   end
 
   def get(pid, id) do
-    ServerProcess.call(pid, {:get, id})
+    ServerProcess.call(pid, {:get_by_id, id})
+  end
+
+  def get(pid, %Date{} = date) do
+    ServerProcess.call(pid, {:get_by_date, date})
   end
 
   def put(pid, %TodoEntry{} = entry) do
-    ServerProcess.call(pid, {:put, entry})
+    ServerProcess.cast(pid, {:put, entry})
   end
 
+  def delete(pid, id) do
+    ServerProcess.cast(pid, {:delete, id})
+  end
+
+  # CALLBACK FUNCTIONS USED INTERNALLY BY THE GENERIC CODE
   def init do
     TodoList.new()
   end
 
-  def handle_call({:get}, state) do
+  def handle_call({:get_all}, state) do
     {TodoList.get_entries(state), state}
   end
 
-  def handle_call({:get, id}, state) do
+  def handle_call({:get_by_id, id}, state) do
     {TodoList.get_entries(state, id), state}
   end
 
-  def handle_call({:put, entry}, state) do
-    {:ok, TodoList.add(state, entry)}
+  def handle_call({:get_by_date, date}, state) do
+    {TodoList.get_entries(state, id), state}
+  end
+
+  def handle_cast({:put, entry}, state) do
+    TodoList.add(state, entry)
+  end
+
+  def handle_cast({:delete, id}, state) do
+    TodoList.delete_entry(state, id)
   end
 end
