fa19c3318fe9d0ab99561ed3ba0ece9c18bae3fe
teberl <mail@teberl.de>
2018-12-22 17:18:01 +0100

    Use a worker pool for database interaction

--- todo_app/lib/todo_app/database.ex
+++ todo_app/lib/todo_app/database.ex
@@ -1,43 +1,44 @@
 defmodule TodoApp.Database do
   use GenServer
 
+  alias TodoApp.DatabaseWorker
+
   @db_folder "./file_storage"
 
   def start, do: GenServer.start(__MODULE__, nil, name: __MODULE__)
-  def get(key), do: GenServer.call(__MODULE__, {:get, key})
-  def store(key, data), do: GenServer.cast(__MODULE__, {:store, key, data})
+
+  def get(key) do
+    key
+    |> choose_worker
+    |> DatabaseWorker.get(key)
+  end
+
+  def store(key, data) do
+    key
+    |> choose_worker
+    |> DatabaseWorker.store(key, data)
+  end
 
   @impl GenServer
   def init(_) do
     File.mkdir_p!(@db_folder)
-    {:ok, nil}
+    {:ok, start_workers()}
   end
 
   @impl GenServer
-  def handle_cast({:store, key, data}, state) do
-    spawn(fn ->
-      key
-      |> file_name()
-      |> File.write!(:erlang.term_to_binary(data))
-    end)
-
-    {:noreply, state}
+  def handle_call({:choose_worker, key}, _from, workers) do
+    worker = :erlang.phash2(key, 3)
+    {:reply, Map.get(workers, worker), workers}
   end
 
-  @impl GenServer
-  def handle_call({:get, key}, caller, state) do
-    spawn(fn ->
-      data =
-        case File.read(file_name(key)) do
-          {:ok, contents} -> :erlang.binary_to_term(contents)
-          _ -> nil
-        end
-
-      GenServer.reply(caller, data)
-    end)
-
-    {:noreply, state}
+  defp choose_worker(key) do
+    GenServer.call(__MODULE__, {:choose_worker, key})
   end
 
-  defp file_name(key), do: Path.join(@db_folder, to_string(key))
+  defp start_workers() do
+    for index <- 0..2, into: %{} do
+      {:ok, pid} = DatabaseWorker.start(@db_folder)
+      {index, pid}
+    end
+  end
 end
